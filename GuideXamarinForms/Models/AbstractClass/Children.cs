acba4e34a4624ebf0dfe4f0b40a98e32d8e4343f
tiagobenaiaspompeo <adonai_90@hotmail.com>
2019-04-07 10:54:22 -0300

    Abstract Class

---
+++ GuideXamarinForms/Models/AbstractClass/Children.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace GuideXamarinForms.Models.AbstractClass
+{
+    public class Children:Father
+    {
+        public override void Call()
+        {
+            Console.WriteLine("Eu chamei");
+        }
+    }
+}
---
+++ GuideXamarinForms/Models/AbstractClass/Father.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace GuideXamarinForms.Models.AbstractClass
+{
+
+    //Pra que serve?
+    //Reuso, diminuir repeticoes de codigos,, diminuir os erros cometidos pelo programador
+
+        //Sem Instancia, Sem multiplas Herancas, funciona como uma super classe, provedor de uma base de definicoes
+
+
+    public abstract class Father
+    {
+        public abstract void Call();
+    }
+}
